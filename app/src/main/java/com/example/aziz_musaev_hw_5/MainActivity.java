a228e98c3d3e915ed594fafa26b8980898a1d3b1
sent1570 <sent.15.01890@gmail.com>
2022-10-19 21:50:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/aziz_musaev_hw_5/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aziz_musaev_hw_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
